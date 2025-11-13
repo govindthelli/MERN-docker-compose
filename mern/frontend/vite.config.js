4b057f13aaee742a1f49710d4d91f6159e218d66
Thelli Govinda Rao <147299435+govindthelli@users.noreply.github.com>
2025-11-13 15:01:03 +0530

    Update vite.config.js

--- mern/frontend/vite.config.js
+++ mern/frontend/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,   // <--- change this to your desired port
+    host: true,   // <--- required for EC2 public access
+  },
 })
