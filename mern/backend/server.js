a384a6ffec01f76c797c33a75094eb5f7a929789
Thelli Govinda Rao <147299435+govindthelli@users.noreply.github.com>
2025-11-16 12:28:25 +0530

    Update server.js

--- mern/backend/server.js
+++ mern/backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cors({
   origin: [
-    "http://98.92.177.168:3000",   // frontend running on 3000
+    "http://98.92.61.219:3000",   // frontend running on 3000
     "http://localhost:3000"      // for local development
   ],
   credentials: true
